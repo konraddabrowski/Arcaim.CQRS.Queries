565e2960bf2eb3d7a35c8171aa9869d321a74e7d
Konrad Dąbrowski <konrad.dabrowski.90@gmail.com>
2021-09-24 12:47:12 +0200

    Update

--- IQueryDispatcher.cs
+++ IQueryDispatcher.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace Arcaim.CQRS.Queries
 {
-    public interface IQueryDispatcher
+    internal interface IQueryDispatcher
     {
         Task<TResult> DispatchAsync<TResult>(IQuery<TResult> query);
         Task<TResult> DispatchAsync<TQuery, TResult>(TQuery query) where TQuery : class, IQuery<TResult>;
